1a12119ccd6ce051081915d2deb1e137683ad421
Matej Mesojedec <matej.mesojedec@iskra-pio.si>
2023-10-27 12:28:15 +0200

    Update

--- program.py
+++ program.py
@@ -9,7 +9,7 @@ window.title("Obdelava HMI screenshotov")
 
 current_directory = os.getcwd()
 poppler_directory = "\\poppler-23.08.0\\Library\\bin"
-logo_directory = "\\pic\\pio_logo.jpg"
+logo_directory = "\\pio_logo.jpg"
 poppler_directory =  current_directory + poppler_directory
 logo_directory = current_directory + logo_directory
 
