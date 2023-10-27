5d7df65a20ece2218a71c13ef1695122c08f37c0
Matej Mesojedec <matej.mesojedec@iskra-pio.si>
2023-10-27 06:58:36 +0200

    Update

--- program.py
+++ program.py
@@ -4,7 +4,7 @@ from tkinter import *
 from tkinter import filedialog
 
 window = Tk()
-window.geometry ("700x210")
+window.geometry ("600x210")
 window.title("Obdelava HMI screenshotov")
 
 current_directory = os.getcwd()
@@ -29,14 +29,15 @@ def pdfToImage():
     for pdf_filename in pdf_filenames:
         images = convert_from_path(pdf_filename, poppler_path=poppler_directory)
         for i, image in enumerate(images):
-            image.save(f"{pdf_filename[:-4]}_{i}.jpg", 'JPEG')
+            image.save(f"{pdf_filename[:-4]}_{i}.png", 'PNG')
 
 def crop():
-    jpg_filenames = glob.glob(pdf_directory + '/*.jpg')  
-    ratio = int(E1.get())
+    jpg_filenames = glob.glob(pdf_directory + '/*.png')  
+    ratio = float(E1.get())
     width_small = 1574/ratio
     height_small = 935/ratio
     small_size = int(width_small), int(height_small)
+    print(ratio)
     for jpg_file in jpg_filenames:
         img = PIL.Image.open(jpg_file)
         width, height = img.size
@@ -45,9 +46,9 @@ def crop():
         right = width - 37
         bottom = 970
         crop = img.crop((left, top, right, bottom))
-        crop.save(jpg_file[:-4] + '_screen_big.jpg', 'JPEG')
+        crop.save(jpg_file[:-4] + '_screen_big.png', 'PNG')
         resize = crop.resize(small_size)
-        resize.save(jpg_file[:-4] + '_screen_small.jpg', 'JPEG')
+        resize.save(jpg_file[:-4] + '_screen_small.png', 'PNG')
         os.remove(jpg_file)
         L3 = Label(window, text='Končano!', fg="blue", font=('Poppins bold', 15))
         L3.place(x=150, y=150)
@@ -64,10 +65,10 @@ L1 = Label(window, text="1. Izberi mapo s PDF-ji", font=('Poppins bold', 15))
 L1.place(x=0, y=5)
 
 
-L2 = Label(window, text="2. Vpiši stopnjo pomanjšanja, npr. 3", font=('Poppins bold', 15))
+L2 = Label(window, text="2. Vpiši stopnjo pomanjšanja, npr. 3.5", font=('Poppins bold', 15))
 L2.place(x=0, y=63)
-E1 = Entry(window, width=10, font=('Poppins bold', 15))
-E1.place(x=320, y=65)
+E1 = Entry(window, width=3, font=('Poppins bold', 15))
+E1.place(x=340, y=65)
 
 L3 = Label(window, text="3. Izvedi proces", font=('Poppins bold', 15))
 L3.place(x=0, y=98)
